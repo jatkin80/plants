cfea09a69b23da60a962803e59d0462f2224b0cf
Julie <jatkin80@ford.com>
2022-06-09 14:14:25 -0500

    database works locally

--- src/main/java/com/juliemarieatkins/plantapi/plant/Plant.java
+++ src/main/java/com/juliemarieatkins/plantapi/plant/Plant.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
+
+import org.hibernate.type.DateType;
+
 import javax.persistence.Id;
 
 import java.sql.Date;
@@ -33,12 +36,12 @@ public class Plant {
   private String description;
 
   @Column(name= "dateadded")
-  private Date dateadded;
+  private DateType dateadded;
 
 @Column(name= "notes")
 private String notes;
 @Column(name= "daystomaturity")
-private Number daysToMaturity;
+private int daysToMaturity;
 @Column(name= "spacing")
 private String spacing;
 
